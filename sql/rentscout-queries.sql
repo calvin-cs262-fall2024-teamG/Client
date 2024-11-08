1e6cd69389e159cc9917f01bd12d5087c78d49e0
TheoPerumal <141761526+TheoPerumal@users.noreply.github.com>
2024-11-07 22:57:15 -0500

    sql subfolder with schema and queries completed

---
+++ sql/rentscout-queries.sql
@@ -0,0 +1,18 @@
+--
+-- This SQL script implements sample queries on the Rentscout database.
+--
+-- @author Theo Perumal
+-- @version Fall, 2024
+--
+
+
+-- get a list of landlords 
+
+
+-- get a list of all properties owned by a Landlord called Erik Kraayveld
+
+
+-- get a list of all properties of price under 2100 
+
+
+-- get the cheapest property listed
